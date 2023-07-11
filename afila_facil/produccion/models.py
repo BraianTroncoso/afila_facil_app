0f72b3a2fb9a0c8c38bad1c5d0679721af0df3ee
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-11 05:23:41 -0300

    [ADD] creacion de modelo Produccion

--- afila_facil/produccion/models.py
+++ afila_facil/produccion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from materias_primas.models import Vidia, Blister, Mango, Carton
 
-# Create your models here.
+class Produccion(models.Model):
+    # Atributos de Produccion
+    vidia = models.ForeignKey(Vidia, on_delete=models.CASCADE)
+    blister = models.ForeignKey(Blister, on_delete=models.CASCADE)
+    mango = models.ForeignKey(Mango, on_delete=models.CASCADE)
+    carton = models.ForeignKey(Carton, on_delete=models.CASCADE)
