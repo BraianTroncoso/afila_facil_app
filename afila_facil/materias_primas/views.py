3cad41cc519c31990f4a5306e0b35cca2999b4e3
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-11 18:02:30 -0300

    [ADD] def eliminar productos

--- afila_facil/materias_primas/views.py
+++ afila_facil/materias_primas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Productos
+from .forms import modificarProductoForm
 
 def nuevo_producto(request):
     if request.POST:
@@ -18,3 +19,9 @@ def mostrar_producto(request):
     productos = Productos.objects.all()
     return render(request, 'productos.html', {'productos': productos, 'mensaje': "No hay Productos"})
 
+
+def eliminarProducto(request, id):
+    Productos.objects.filter(pk=id).delete()
+    mensaje = "Producto eliminado."
+    productos =  Productos.objects.all()
+    return render (request, 'productos.html',{'productos':productos, 'mensaje':mensaje})
