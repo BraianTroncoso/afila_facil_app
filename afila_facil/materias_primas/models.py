5d52be3a10b05a27406d5ec7ed6d35250fbb2cc8
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-11 15:53:49 -0300

    [REF] materias primas - modelo Productos

--- afila_facil/materias_primas/models.py
+++ afila_facil/materias_primas/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Prodcuto(models.Model):
+class Productos(models.Model):
+    nombre = models.CharField(max_length=45)
+    descripcion = models.CharField(max_length=200)
+    precio = models.IntegerField()
     cantidad = models.IntegerField()
+
+    class Meta:
+        db_table = 'productos'
+        verbose_name = "Producto"
+
+    def __str__(self):
+        return f'''
+            {self.nombre},
+            {self.descripcion},
+            {self.cantidad},
+            {self.precio}        
+        '''    
