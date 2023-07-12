13fcaa6b06595aea043899d35797d8ac502d7901
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-12 04:34:57 -0300

    [ADD] crud completo ok

--- afila_facil/materias_primas/views.py
+++ afila_facil/materias_primas/views.py
@@ -23,5 +23,23 @@ def mostrar_producto(request):
 def eliminarProducto(request, id):
     Productos.objects.filter(pk=id).delete()
     mensaje = "Producto eliminado."
-    productos =  Productos.objects.all()
+    productos = Productos.objects.all()
     return render (request, 'productos.html',{'productos':productos, 'mensaje':mensaje})
+
+
+def mostrarFormularioProducto(request, id):
+    productos = Productos.objects.only(id =id)
+    return render(request, 'altaProducto.html', {'productos': productos})
+    pass
+
+def modificarProducto(request, id):
+    if request.method == 'POST':
+            nombre = request.POST['nombre']
+            descripcion = request.POST['direccion']
+            precio = request.POST['precio']
+            cantidad = request.POST['precio']
+            img = request.POST['img']
+            
+            productos =  Productos(id=id,nombre=nombre, precio=precio, cantidad=cantidad, descripcion=descripcion, imagen=img)
+            productos.save()
+            return redirect('productos')
