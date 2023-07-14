257eb88a661919d5fd12c1da7511ef8bdbaccb12
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-14 05:32:58 -0300

    [REFACTOR] modificar

--- afila_facil/materias_primas/forms.py
+++ afila_facil/materias_primas/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Productos
     
-class FormProducto(forms.Form):
+class ProductoForm(forms.Form):
     nombre = forms.CharField(max_length=100, required=True)
     descripcion = forms.CharField(max_length=255, required=True, widget=forms.Textarea)
     precio = forms.IntegerField(required=True)
--- afila_facil/materias_primas/views.py
+++ afila_facil/materias_primas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Productos
-from .forms import FormProducto
+from .forms import ProductoForm
 
 def nuevo_producto(request):
     if request.POST:
@@ -27,18 +27,31 @@ def eliminar_producto(request, id):
     return render (request, 'productos.html',{'productos':productos, 'mensaje':mensaje})
 
 
-
 def editar_producto(request, id):
-     if request.POST:
-        productos = get_object_or_404(Productos.objects.filter(pk=id))
-        nombre = request.POST['nombre']
-        precio = request.POST['precio']
-        cantidad = request.POST['cantidad']
-        descripcion = request.POST['descripcion']
-        imagen = request.POST['imagen']
-        productos = Productos(nombre=nombre, precio=precio, cantidad=cantidad, descripcion=descripcion, imagen=imagen)
-        productos.save()
-        return render(request, 'productos')
+    print("ID:", id)
+    producto = get_object_or_404(Productos, pk=id)
+    print("Producto:", producto)
+
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        precio = request.POST.get('precio')
+        cantidad = request.POST.get('cantidad')
+        descripcion = request.POST.get('descripcion')
+        imagen = request.POST.get('imagen')
+
+        print("Datos del formulario:", nombre, precio, cantidad, descripcion, imagen)
+
+        producto.nombre = nombre
+        producto.precio = precio
+        producto.cantidad = cantidad
+        producto.descripcion = descripcion
+        producto.imagen = imagen
+        producto.save()
+
+        # Redireccionar a la página de confirmación o a la página de detalles del producto actualizado
+        return render(request, 'modificacionProducto.html')
+
+    return render(request, 'modificacionProducto.html')
 
 # def mostrarFormularioProducto(request, id):
 #     productos = Productos.objects.only(id =id)
