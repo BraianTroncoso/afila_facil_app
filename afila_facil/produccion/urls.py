1e21f5d2cb48af1c94074caf925c2624b411d615
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-18 04:10:56 -0300

    [ADD] import urls materias_primas - produccion

--- afila_facil/produccion/urls.py
+++ afila_facil/produccion/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from produccion import views
-
+from materias_primas import urls,views,templates
 
 urlpatterns = [
     path('', views.mostrar_produccion, name='produccion')
