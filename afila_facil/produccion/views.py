ca628367d3883703e98e7fd8962aef2c2cfea60e
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-18 04:07:57 -0300

    [ADD] views def  mostrar_produccion  produccion

--- afila_facil/produccion/views.py
+++ afila_facil/produccion/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Produccion
 
 # Create your views here.
+def mostrar_produccion(request):
+    produccion = Produccion.objects.all()
+    return render(request, 'produccion.html', {'produccion': produccion, 'mensaje': "No hay Productos en Produccion"})
