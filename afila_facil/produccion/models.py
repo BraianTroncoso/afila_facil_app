6a72d7f4822b089137c6c88f8ab800252bd71adb
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-19 03:46:41 -0300

    [ADD] def crear_instancia actualizada

--- afila_facil/produccion/models.py
+++ afila_facil/produccion/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from materias_primas.models import Productos
+
 class Produccion(models.Model):
     producto_completo = models.BooleanField(default=False)
-    productos = models.ForeignKey(Productos, on_delete=models.CASCADE, related_name='produccion')
+    productos = models.ForeignKey(Productos, on_delete=models.CASCADE, related_name='produccion', null=True)
 
-    
     class Meta:
         db_table = 'produccion'
         verbose_name = "Produccion"
 
     def __str__(self):
         return f"{self.producto_completo}"
-    
--- afila_facil/produccion/views.py
+++ afila_facil/produccion/views.py
@@ -11,22 +11,22 @@ def mostrar_produccion(request):
 def crear_instancia_produccion(request):
     if request.method == 'POST':
         cantidad = request.POST.get('cantidad')
+        print('Se recibió una solicitud POST')
+        print(cantidad)
         if cantidad is not None:
             cantidad = int(cantidad)
+            productos = Productos.objects.all()
+
+            for producto in productos:
+                producto.cantidad -= cantidad
+                producto.save()
         else:
             cantidad = 0
-
-        productos = Productos.objects.all()
-        for producto in productos:
-            producto.cantidad -= cantidad
-            producto.save()
-
-        # Obtén un objeto Productos específico para asignarlo al campo productos en Produccion
-        producto_referencia = get_object_or_404(Productos, id=1)
-
-        produccion = Produccion.objects.create(producto_completo=True, productos=producto_referencia)
-        produccion.save()
+        
+        # Resto del código para crear una instancia de Produccion
 
         return redirect('produccion')
     else:
         return redirect('produccion')
+
+
