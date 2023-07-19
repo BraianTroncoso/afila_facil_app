8e019ad5945fd745377ab6d1370448471f30c74b
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-19 00:44:32 -0300

    [ADD] if produccion.html - boton

--- afila_facil/produccion/views.py
+++ afila_facil/produccion/views.py
@@ -4,4 +4,5 @@ from .models import Produccion
 # Create your views here.
 def mostrar_produccion(request):
     print('Hola produccion')
+    produccion = Produccion.objects.all()
     return render(request,'produccion.html',{'produccion': produccion, 'mensaje': "No hay Productos en Produccion"})
