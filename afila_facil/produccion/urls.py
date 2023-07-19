c521e9e4bca04d6805898ffa5d5f3a6f6a36547f
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-19 05:02:09 -0300

    [ADD] def eliminar_produccion

--- afila_facil/produccion/urls.py
+++ afila_facil/produccion/urls.py
@@ -4,5 +4,6 @@ from produccion import views
 
 urlpatterns = [
     path('', views.mostrar_produccion, name='produccion'),
-    path('crear_instancia_produccion/',views.crear_instancia_produccion, name='crear_instancia_produccion')
+    path('crear_instancia_produccion/',views.crear_instancia_produccion, name='crear_instancia_produccion'),
+    path('eliminar_produccion/',views.eliminar_produccion, name='eliminar_produccion')
 ]
--- afila_facil/produccion/views.py
+++ afila_facil/produccion/views.py
@@ -38,3 +38,20 @@ def crear_instancia_produccion(request):
         return redirect('produccion')
     else:
         return redirect('produccion')
+
+
+
+def eliminar_produccion(request):
+    if request.method == 'POST':
+        produccion = Produccion.objects.first()
+    
+        produccion.produccion_cantidad -=1
+        produccion.save()
+
+        productos = Productos.objects.all()
+        for producto in productos:
+                producto.cantidad +=1
+                producto.save()
+        return redirect('produccion')
+    else:
+        return redirect('produccion')    
