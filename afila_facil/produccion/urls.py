4302a1423041c894c3c0bde324c34ce3e62176ec
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-23 01:45:13 -0300

    [ADD] url finalizar en produccion

--- afila_facil/produccion/urls.py
+++ afila_facil/produccion/urls.py
@@ -5,5 +5,6 @@ from produccion import views
 urlpatterns = [
     path('', views.mostrar_produccion, name='produccion'),
     path('crear_instancia_produccion/',views.crear_instancia_produccion, name='crear_instancia_produccion'),
-    path('eliminar_produccion/',views.eliminar_produccion, name='eliminar_produccion')
+    path('eliminar_produccion/',views.eliminar_produccion, name='eliminar_produccion'),
+    path('finalizar_todos_produccion', views.finalizar_todos_produccion, name='finalizar_todos_produccion')
 ]
