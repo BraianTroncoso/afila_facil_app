043239c865e5324e93545faf2f57442b9561c185
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-23 01:02:16 -0300

    [ADD] def eliminar_product - eliminar precio

--- afila_facil/produccion/views.py
+++ afila_facil/produccion/views.py
@@ -40,21 +40,25 @@ def crear_instancia_produccion(request):
         return redirect('produccion')
 
 
-
 def eliminar_produccion(request):
     if request.method == 'POST':
         produccion = Produccion.objects.first()
     
-        produccion.produccion_cantidad -=1
+        produccion.produccion_cantidad -= 1
         produccion.save()
 
         productos = Productos.objects.all()
         for producto in productos:
-                producto.cantidad +=1
-                producto.save()
+            producto.cantidad += 1
+            producto.save()
+            produccion.produccion_total -= producto.precio
+                
+        produccion.save()
+        
         return redirect('produccion')
     else:
-        return redirect('produccion')    
+        return redirect('produccion')
+
 
 def eliminar_todos_produccion(request):
     pass        
