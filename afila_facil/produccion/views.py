3bf36fd76fd66b185031de679c740924be32fa2f
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-27 17:52:42 -0300

    [REFACTOR] solucion bug gral

--- afila_facil/produccion/views.py
+++ afila_facil/produccion/views.py
@@ -71,7 +71,6 @@ def finalizar_todos_produccion(request):
 
         if produccion and produccion.productos:
             while produccion.productos and produccion.produccion_cantidad > 0:
-                print('bucle ok')
                 produccion.produccion_cantidad -= 1
                 produccion.save()
                 for producto in productos:
