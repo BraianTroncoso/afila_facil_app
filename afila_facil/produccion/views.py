a99434baf7ee3b868f3606c97ae77d5bdd484942
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-26 23:44:14 -0300

    [FIX] debug def finalizar

--- afila_facil/produccion/views.py
+++ afila_facil/produccion/views.py
@@ -60,22 +60,22 @@ def eliminar_produccion(request):
         return redirect('produccion')
 
 
-def finalizar_todos_produccion(request):
-    if request.method == 'POST':
-        produccion = Produccion.objects.first()
+# def finalizar_todos_produccion(request):
+#     if request.method == 'POST':
+#         produccion = Produccion.objects.first()
 
-        if produccion:
-            productos = produccion.productos.all()
+#         if produccion:
+#             productos = produccion.productos.all()
 
-            if productos:
-                for producto in productos:
-                    produccion.produccion_total -= producto.precio
-                    producto.cantidad += 1
-                    producto.save()
-                    print('blucle ok')
-                produccion.produccion_cantidad -= 1
-                produccion.save()
+#             if productos:
+#                 for producto in productos:
+#                     produccion.produccion_total -= producto.precio
+#                     producto.cantidad += 1
+#                     producto.save()
+#                     print('blucle ok')
+#                 produccion.produccion_cantidad -= 1
+#                 produccion.save()
 
-        return redirect('produccion')
-    else:
-        return redirect('produccion')
+#         return redirect('produccion')
+#     else:
+#         return redirect('produccion')
