cba1d16dc6e701da7e7e0172c003ea77be80bfc6
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-28 00:35:27 -0300

    [REFACTOR] codigo nuevo usuario

--- afila_facil/usuarios/models.py
+++ afila_facil/usuarios/models.py
@@ -1,15 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
-
-
-class Usuario(AbstractUser):
-    password = models.CharField(max_length=50, default='123456abc!')
-    username = models.CharField(max_length=50, default='username', unique=True)
-    def __str__(self):
-        return f'''
-            {self.first_name} 
-            {self.last_name}       
-        '''
-
-    
