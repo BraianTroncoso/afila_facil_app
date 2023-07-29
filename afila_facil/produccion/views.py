fb8e4f58cb22d5e97780cb4b2f9b1152b31f9cd2
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-29 16:00:50 -0300

    [FIX] actualizacion código gral

--- afila_facil/produccion/views.py
+++ afila_facil/produccion/views.py
@@ -3,51 +3,55 @@ from .models import Produccion
 from materias_primas.models import Materias
 
 # Create your views here.
-def mostrar_produccion(request):
-    produccion = Produccion.objects.all()#Obtengo una sola instsancia en vez de todas
-    return render(request,'produccion.html',{'produccion': produccion, 'mensaje': "No hay Materiasen Produccion"})
 
+
+def mostrar_produccion(request):
+    # Obtengo una sola instsancia en vez de todas
+    produccion = Produccion.objects.all()
+    return render(request, 'produccion.html', {'produccion': produccion, 'mensaje': "No hay Materiasen Produccion"})
 
 
 def crear_instancia_produccion(request):
     if request.method == 'POST':
         cantidad = request.POST.get('cantidad')
 
-        if cantidad is not None:
+        if cantidad is not None and cantidad != '':
             cantidad = int(cantidad)
-            productos = Materias.objects.all()
-
-            #Linea agg por bug - necesito tener mas de una instancia para no tener problemas en la foreignkey
-            #produccion = Produccion.objects.create(producto_completo=True, productos=productos[0])
-            for producto in productos:
-                producto.cantidad -= cantidad
-                producto.save()
-
-            produccion = Produccion.objects.first()
-            if not produccion:
-                produccion = Produccion.objects.create(producto_completo=True)
-
-            produccion.produccion_cantidad += cantidad
-            produccion.save()
-
-            total = sum(producto.precio * cantidad for producto in productos)
-            produccion.produccion_total += total
-            produccion.save()
-
         else:
             cantidad = 0
+            return redirect('produccion')
+
+        materias = Materias.objects.all()
+
+        for materia in materias:
+            materia.cantidad -= cantidad
+            materia.save()
+
+        produccion = Produccion.objects.first()
+        if not produccion:
+            produccion = Produccion.objects.create(producto_completo=True)
+
+        produccion.produccion_cantidad += cantidad
+        produccion.save()
+
+        total = sum(materia.precio * cantidad for materia in materias)
+        produccion.produccion_total += total
+        produccion.save()
 
         return redirect('produccion')
     else:
         return redirect('produccion')
 
+
 # VER ESTA PARTE DEL CÓDIGO NUEVAMENTE PORQUE APARENTEMENTE SI SE AGREGA UN PRODUCTO DE CADA UNO
 # PERO CUANDO VOY A FINALIZAR TODOS, ESTOS ESTAN RELACIONADOS A UN SOLO ID, ENTONCES CUANDO FINALIZA TODO
 # RETORNA LA CANTIDAD SOLAMENTE AL ID 1 Y LOS DEMAS PRODUCTOS NO RECIBEN EL AUMENTO
+
+
 def eliminar_produccion(request):
     if request.method == 'POST':
         produccion = Produccion.objects.first()
-    
+
         produccion.produccion_cantidad -= 1
         produccion.save()
 
@@ -56,9 +60,9 @@ def eliminar_produccion(request):
             producto.cantidad += 1
             producto.save()
             produccion.produccion_total -= producto.precio
-                
+
         produccion.save()
-        
+
         return redirect('produccion')
     else:
         return redirect('produccion')
@@ -77,10 +81,8 @@ def finalizar_todos_produccion(request):
                     materia.cantidad += 1
                     materia.save()
                     produccion.produccion_total -= materia.precio
-                
-        produccion.save()    
+
+        produccion.save()
         return redirect('produccion')
     else:
         return redirect('produccion')
-
-
