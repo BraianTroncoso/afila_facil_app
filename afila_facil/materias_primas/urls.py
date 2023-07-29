90634e2efabb557cbf20740e32c329100bf30c38
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-29 14:24:37 -0300

    [FIX] actualizacion código gral

--- afila_facil/materias_primas/urls.py
+++ afila_facil/materias_primas/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.mostrar_materias_primas, name='materias_primas'),
     path('nuevo_producto/', views.nuevo_producto, name='nuevo_producto'),
     path('editar/<int:id>/', views.editar_producto, name='editar_producto'),
-    path('eliminar/<int:id>/', views.eliminar_producto, name='eliminar_producto'),
+    path('eliminar/<int:id>/', views.eliminar_materia, name='eliminar_materia'),
     path('clientes/', views.clientes, name='clientes')
 ]
