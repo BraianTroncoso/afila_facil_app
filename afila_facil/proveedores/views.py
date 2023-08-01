ed5ad34dc4b86b59d2c65abfa0fa08a828f6d24f
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-01 01:20:16 -0300

    [ADD] def nuevo_proveedor

--- afila_facil/proveedores/views.py
+++ afila_facil/proveedores/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def nuevo_proveedor(request):
+    return render(request, 'nuevo_proveedor.html')
 
 def proveedores(request):
     return render(request,'proveedores.html')
