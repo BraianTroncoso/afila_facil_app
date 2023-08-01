2844b0606e8ecb61d6043746966452d1ea03a5e1
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-31 21:18:49 -0300

    [UPDATE] def crear_instancia_produccion import messages setting

--- afila_facil/afila_facil/settings.py
+++ afila_facil/afila_facil/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,6 +53,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-dark',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 ROOT_URLCONF = 'afila_facil.urls'
 
 TEMPLATES = [
