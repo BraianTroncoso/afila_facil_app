f7524fe94193fcefafdd096aeac2dd65d9bbe583
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-01 01:21:05 -0300

    [ADD] url  nuevo_proveedor

--- afila_facil/proveedores/urls.py
+++ afila_facil/proveedores/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from proveedores import views
 
 urlpatterns = [
-    path('proveedores/', views.proveedores, name='proveedores')
+    path('proveedores/', views.proveedores, name='proveedores'),
+    path('nuevo_proveedor/',views.nuevo_proveedor, name='nuevo_proveedor')
 ]
