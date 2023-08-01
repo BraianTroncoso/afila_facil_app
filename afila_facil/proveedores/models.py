879d5326f42fc6f79493bf1de6fc76644f1bad23
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-01 02:29:08 -0300

    [ADD] campo models proveedor - img

--- afila_facil/proveedores/models.py
+++ afila_facil/proveedores/models.py
@@ -8,9 +8,14 @@ class Proveedores(models.Model):
     direccion = models.CharField(max_length=100)
     tel = models.CharField(max_length=500)
     email = models.CharField(max_length=100)
-    imagen = models.CharField(max_length=100) 
+    imagen = models.TextField(null=True, blank=True)
     produccion = models.ForeignKey(Produccion, on_delete=models.CASCADE, related_name='produccion', null=True)
 
+    def get_imagen_url(self):
+        if self.imagen:
+            return f"/static/img/{self.imagen}"
+        else:
+            return "/static/img/afilador.jpeg"
 
     class Meta:
         db_table = 'proveedores'
