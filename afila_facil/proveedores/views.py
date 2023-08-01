04c6f218ede2efd7734a9bff209e20047b769ba7
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-01 02:33:37 -0300

    [ADD] forms.py - proveedores

--- afila_facil/proveedores/views.py
+++ afila_facil/proveedores/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def nuevo_proveedor(request):
-    return render(request, 'nuevo_proveedor.html')
+    if request.method == 'POST':
+        
+
+        return render(request, 'nuevo_proveedor.html')
 
 def proveedores(request):
     return render(request,'proveedores.html')
