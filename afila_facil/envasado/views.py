665f8e7bc9372cf4cbc0cf507727fe74bbd919e7
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-05 06:09:17 -0300

    [ADD] nuevo_envasado.html - app - envasado

--- afila_facil/envasado/views.py
+++ afila_facil/envasado/views.py
@@ -8,3 +8,4 @@ from django.contrib import messages
 def mostrar_envasado(request):
     envasado = Envasado.objects.all()
     return render(request, 'mostrar_envasado.html', {'envasado': envasado, 'mensaje': "No hay Tipo de envasado disponible"})
+
