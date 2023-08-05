76b2832e62920b3caffc6cf36437af97c6baad24
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-05 05:57:15 -0300

    [ADD] models Envasado - app - envasado

--- afila_facil/envasado/models.py
+++ afila_facil/envasado/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from produccion.models import Produccion
 
-# Create your models here.
+class Envasado(models.Model):
+    nombre = models.CharField(max_length=100, default='')
+    produccion = models.ForeignKey(Produccion, on_delete=models.CASCADE, related_name='envasado', null=True)
+    cantidad = models.IntegerField(default=0)
+    total = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'envasado'
+        verbose_name = "envasado"
+
+    def __str__(self):
+        return f"{self.nombre}"
+        
+    
+
