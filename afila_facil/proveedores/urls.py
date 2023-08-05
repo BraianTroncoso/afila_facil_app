f17309268cbeb26abb9dc406fe2358a922cbdcc1
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-05 04:11:31 -0300

    [ADD] def eliminar_proveedor ok

--- afila_facil/proveedores/urls.py
+++ afila_facil/proveedores/urls.py
@@ -4,5 +4,5 @@ from proveedores import views
 urlpatterns = [
     path('', views.mostrar_proveedores, name='proveedores'),
     path('nuevo_proveedor/',views.nuevo_proveedor, name='nuevo_proveedor'),
-    path('eliminar/<int:id>/', views.eliminar_proveedor, name='eliminar_provedor')
+    path('eliminar/<int:id>/', views.eliminar_proveedor, name='eliminar_proveedor')
 ]
