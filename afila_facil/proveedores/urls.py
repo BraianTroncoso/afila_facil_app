6fc15a5c576bd2d6299d157c3baa78c69aa39bac
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-05 04:18:51 -0300

    [ADD] urls - provedor editar

--- afila_facil/proveedores/urls.py
+++ afila_facil/proveedores/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.mostrar_proveedores, name='proveedores'),
     path('nuevo_proveedor/',views.nuevo_proveedor, name='nuevo_proveedor'),
     path('eliminar/<int:id>/', views.eliminar_proveedor, name='eliminar_proveedor'),
-    path('editar/<int:id>/', views.editar_materia, name='editar_proveedor')
+    path('editar/<int:id>/', views.editar_proveedor, name='editar_proveedor')
 ]
