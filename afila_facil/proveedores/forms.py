519cca8a22bd4a9021aa8ba55985d6c2086f521d
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-06 23:07:59 -0300

    [REFACTOR] campo apellido por detalle - proveedores - app

--- afila_facil/proveedores/forms.py
+++ afila_facil/proveedores/forms.py
@@ -3,7 +3,11 @@ from .models import Proveedores
     
 class ProveedoresForm(forms.Form):
     nombre = forms.CharField(max_length=100, required=True)
-    detalle = forms.CharField(max_length=200, required=False)
+    detalle = forms.CharField(
+        max_length=200,
+        required=False,
+        widget=forms.Textarea(attrs={'class': 'form-control'})
+    )
     direccion = forms.CharField(max_length=100, required=True)
     telefono = forms.CharField(max_length=100, required=True)
     email = forms.CharField(max_length=100, required=True)
