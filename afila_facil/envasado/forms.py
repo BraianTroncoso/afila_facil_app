47d19f8f09fcae5420dce7a54ac894b91028f204
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-09 22:16:28 -0300

    [ADD] forms - envasado

--- afila_facil/envasado/forms.py
+++ afila_facil/envasado/forms.py
@@ -3,4 +3,6 @@ from produccion.models import Produccion
 from .models import Envasado
 
 class EnvasadoForm(forms.Form):
-    pass
+    cantidad = forms.IntegerField()
+    produccion_choices = [(produccion.id, produccion.nombre) for produccion in Produccion.objects.all()]
+    produccion_seleccionada = forms.MultipleChoiceField(choices=produccion_choices, widget=forms.CheckboxSelectMultiple())
