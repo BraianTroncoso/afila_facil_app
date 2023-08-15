be6581a25b496f6acfe90d44ebc868fe516a3043
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-15 01:50:33 -0300

    [ADD] def editar_envasado - envasado

--- afila_facil/envasado/urls.py
+++ afila_facil/envasado/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.mostrar_envasado, name='mostrar_envasado'),
     path('nuevo_envasado/', views.nuevo_envasado, name='nuevo_envasado'),
     path('agregar_sub_producto/<int:id>/', views.agregar_sub_producto, name='agregar_sub_producto'),
-    path('editar_envasado/',views.editar_envasado, name='editar_envasado')
+    path('editar_envasado/<int:id>',views.editar_envasado, name='editar_envasado')
 ]
