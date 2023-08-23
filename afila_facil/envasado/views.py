f7a2bd2e26daf7988de39fd2cc99d55cbec2a69c
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-23 17:25:43 -0300

    [ADD] act codigo

--- afila_facil/envasado/views.py
+++ afila_facil/envasado/views.py
@@ -90,3 +90,5 @@ def editar_envasado(request, id):
 # Volver a ver la lógica, seguro hay un error con el tema del producto o editar, igual, tampoco puedo seguir sino se si tiene un precio
 # total o que.
 # Tambien ver el bug de cuando se selecciona un elemento a, este mismo supera los 100vh y rompe todo.
+# Me volvi a fijar y sigue todo igual, tengo que volver a ver la lógica para ver como puedo finalizarlo
+# Pero tengo que esperar al Lisandro a ver que me dice
