4e4670e6ce9dfd715e31fb5c8d6dcf0c851b5a7d
Braian <troncosobraianaxel@gmail.com>
2023-09-10 16:19:05 -0300

    [ADD] def nuevo_cliente

--- afila_facil/clientes/views.py
+++ afila_facil/clientes/views.py
@@ -4,3 +4,24 @@ from .models import Clientes
 # Create your views here.
 def clientes(request):
     return render(request,'clientes.html')
+
+
+def nuevo_cliente(request):
+    if request.method == 'POST':
+        form = ClientesForm(request.POST, request.FILES)
+        if form.is_valid():
+            nombre = form.cleaned_data['nombre']
+            detalle = form.cleaned_data['detalle']
+            direccion = form.cleaned_data['direccion']
+            telefono = form.cleaned_data['telefono']
+            email = form.cleaned_data['email']
+            imagen = form.cleaned_data['imagen']
+
+            clientes = Clientes(nombre=nombre, detalle=detalle,
+                                  direccion=direccion, telefono=telefono, email=email,
+                                  imagen=imagen)
+            clientes.save()
+            return redirect('clientes')
+        else:
+            form = ClientesForm()    
+    return render(request, 'nuevo_cliente.html')
