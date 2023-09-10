56a8c104e0e4d1243ef45c3249a42c5fe39ca0b2
Braian <troncosobraianaxel@gmail.com>
2023-09-10 16:07:11 -0300

    [ADD] creacion archivo url.py - clientes

--- afila_facil/materias_primas/urls.py
+++ afila_facil/materias_primas/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.mostrar_materias_primas, name='materias_primas'),
     path('nueva_materia/', views.nueva_materia, name='nueva_materia'),
     path('editar/<int:id>/', views.editar_materia, name='editar_materia'),
-    path('eliminar/<int:id>/', views.eliminar_materia, name='eliminar_materia'),
-    path('clientes/', views.clientes, name='clientes')
+    path('eliminar/<int:id>/', views.eliminar_materia, name='eliminar_materia')
 ]
