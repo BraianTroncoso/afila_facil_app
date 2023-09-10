e312b70959a28baa186ec93d0325f6112673aa74
Braian <troncosobraianaxel@gmail.com>
2023-09-10 16:20:25 -0300

    [ADD] clientes - forms

---
+++ afila_facil/clientes/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Clientes
+
+class ClientesForm(forms.Form):
+    nombre = forms.CharField(max_length=100, required=True)
+    detalle = forms.CharField(
+        max_length=200,
+        required=False,
+        widget=forms.Textarea(attrs={'class': 'form-control'})
+    )
+    direccion = forms.CharField(max_length=100, required=True)
+    telefono = forms.CharField(max_length=100, required=True)
+    email = forms.CharField(max_length=100, required=True)
+    imagen = forms.CharField(max_length=100, required=False)
+
+    class Meta:
+            model = Clientes
