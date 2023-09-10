69fea7627dbb1537a8bc8c9ea4374be627205d9b
Braian <troncosobraianaxel@gmail.com>
2023-09-10 16:47:12 -0300

    [ADD] act codigo - app - clientes

--- afila_facil/clientes/urls.py
+++ afila_facil/clientes/urls.py
@@ -4,5 +4,7 @@ from clientes import views
 urlpatterns = [
     path('', views.clientes, name='clientes'),
     path('nuevo_cliente', views.nuevo_cliente, name='nuevo_cliente'),
-    path('editar/<int:id>/', views.editar_cliente, name='editar_cliente')
+    path('editar/<int:id>/', views.editar_cliente, name='editar_cliente'),
+    path('eliminar/<int:id>/', views.eliminar_cliente, name='eliminar_cliente')
+    
 ]
