94d9c18b5cba44a885f09007c0af1e5a5451e83d
Braian <troncosobraianaxel@gmail.com>
2023-09-10 23:31:24 -0300

    [ADD] act código - app ventas

--- afila_facil/ventas/views.py
+++ afila_facil/ventas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Ventas
 from .forms import VentasForm
 # Create your views here.
-def mostrar_proveedores(request):
+def mostrar_ventas(request):
     ventas = Ventas.objects.all()
-    return render(request,'ventas.html',{'ventas': ventas, 'mensaje': "No hay Proveedores"})
+    return render(request,'ventas.html',{'ventas': ventas, 'mensaje': "No hay Ventas"})
 
