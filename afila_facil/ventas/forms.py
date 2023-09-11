bb6d7d729a2bcde14be1447e88a7f05875298a85
Braian <troncosobraianaxel@gmail.com>
2023-09-10 23:53:28 -0300

    [ADD] act código - app ventas

--- afila_facil/ventas/forms.py
+++ afila_facil/ventas/forms.py
@@ -1,5 +1,12 @@
 from django import forms
-from .models import Ventas
-    
+from produccion.models import Produccion
+from clientes.models import Clientes
+
 class VentasForm(forms.Form):
-   pass
+   nombre = forms.CharField(max_length=100)
+   clientes = forms.ModelChoiceField(queryset=Clientes.objects.all())
+   produccion = forms.ModelChoiceField(queryset=Produccion.objects.all())
+   precio = forms.IntegerField()
+   cantidad = forms.IntegerField()
+   imagen = forms.CharField(max_length=100, required=False)
+
