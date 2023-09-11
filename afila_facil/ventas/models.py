60097b3e7b0833e1c75065904a1670646810b473
Braian <troncosobraianaxel@gmail.com>
2023-09-10 23:18:04 -0300

    [ADD] creacion archivo class - ventas

--- afila_facil/ventas/models.py
+++ afila_facil/ventas/models.py
@@ -5,5 +5,8 @@ from produccion.models import Produccion
 # Create your models here.
 class Ventas(models.Model):
     cantidad = models.IntegerField(default=0)
+    detalle = models.CharField(max_length=200, blank=True)
     produccion= models.ForeignKey(Produccion, on_delete=models.CASCADE)
     clientes = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    costo = models.IntegerField(default=0)
+    total = models.IntegerField(default=0)
